e826f51b1aa063ae4e6b460ef2e8f579d2f723d0
zaza <154515300+zazaiu@users.noreply.github.com>
2025-09-10 11:56:16 +0300

    Update server.go

--- space_astrophysics/internal/api/server.go
+++ space_astrophysics/internal/api/server.go
@@ -2,9 +2,6 @@ package api
 
 import (
 	"log"
-	"net/http"
-	"time"
-
 	"space_astrophysics/internal/app/handler"
 	"space_astrophysics/internal/app/repository"
 
@@ -12,31 +9,28 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var lines = []string{"first line", "second line", "third line", "fourth line"}
-
-func HelloHandler(ctx *gin.Context) {
-	ctx.HTML(http.StatusOK, "index.html", gin.H{
-		"time":   time.Now().Format("15:04:05"),
-		"massiv": lines,
-	})
-}
 func StartServer() {
 	log.Println("Starting server")
+
 	repo, err := repository.NewRepository()
 	if err != nil {
 		logrus.Error("ошибка инициализации репозитория")
+		return
 	}
 
-	handler := handler.NewHandler(repo)
+	h := handler.NewHandler(repo)
 
 	r := gin.Default()
-
 	r.LoadHTMLGlob("templates/*")
 	r.Static("/static", "./resources")
 
-	r.GET("/Hello", handler.GetOrders)
-	r.GET("/order/:id", handler.GetOrder) // вот наш новый обработчик
+	r.GET("/services", h.GetServices)
+	r.GET("/services/:id", h.GetService)
+	r.GET("/order/:id", h.GetOrder)
+	r.GET("/order/add/:id", h.AddToOrder)
+	r.GET("/order/clear/:id", h.ClearOrder)
+	r.GET("/order/calc/:id", h.CalcOrder)
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	r.Run(":8080")
 	log.Println("Server down")
 }
