50f55886ef7f5012b1df3046194762a9409fd915
zaza <154515300+zazaiu@users.noreply.github.com>
2025-09-22 15:36:39 +0300

    Update server.go

--- space_astrophysics/internal/api/server.go
+++ space_astrophysics/internal/api/server.go
@@ -1,36 +1,45 @@
 package api
 
 import (
+	"html/template"
 	"log"
+	"net/http"
+
 	"space_astrophysics/internal/app/handler"
 	"space_astrophysics/internal/app/repository"
 
 	"github.com/gin-gonic/gin"
-	"github.com/sirupsen/logrus"
 )
 
 func StartServer() {
-	log.Println("Starting server")
-
 	repo, err := repository.NewRepository()
 	if err != nil {
-		logrus.Error("ошибка инициализации репозитория")
-		return
+		log.Fatal(err)
 	}
-
 	h := handler.NewHandler(repo)
 
 	r := gin.Default()
+
+	// ✅ доверяем только localhost, убираем warning
+	r.SetTrustedProxies([]string{"127.0.0.1"})
+
+	// подключаем шаблоны
+	r.SetFuncMap(template.FuncMap{})
 	r.LoadHTMLGlob("templates/*")
-	r.Static("/static", "./resources")
+	r.Static("/static", "./resourse")
 
-	r.GET("/services", h.GetServices)
-	r.GET("/services/:id", h.GetService)
-	r.GET("/order/:id", h.GetOrder)
-	r.GET("/order/add/:id", h.AddToOrder)
-	r.GET("/order/clear/:id", h.ClearOrder)
-	r.GET("/order/calc/:id", h.CalcOrder)
+	// ==== маршруты ====
+	r.GET("/planets", h.ListPlanets)
+	r.GET("/planets/:id", h.ShowPlanetDetail)
 
+	r.GET("/world/:id", h.ViewMissionOrder)
+	r.GET("/world/add/:id", h.AddToOrder)
+
+	// redirect корня на список планет
+	r.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusFound, "/planets")
+	})
+
+	
 	r.Run(":8080")
-	log.Println("Server down")
 }
